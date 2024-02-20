b7ec1bb27173a11604ec762413c3d054bff8fa5b
PosikBoy <z.kalinichenko4@gmail.com>
2024-02-20 07:27:25 +0300

    kasm

--- js/myaddresses.js
+++ js/myaddresses.js
@@ -1,45 +1,49 @@
-const addressMenuOptions = document?.querySelectorAll(".my-addresses__option");
+window.addEventListener("DOMContentLoaded", () => {
+  const addressMenuOptions = document?.querySelectorAll(
+    ".my-addresses__option"
+  );
 
-addressMenuOptions.forEach(function (option) {
-  option.addEventListener("click", function () {
-    addressMenuOptions.forEach(function (item) {
-      item.classList.remove("selected");
-      let saveButton = item.querySelector(".my-addresses__save");
-      saveButton.classList.remove("active");
+  addressMenuOptions.forEach(function (option) {
+    option.addEventListener("click", function () {
+      addressMenuOptions.forEach(function (item) {
+        item.classList.remove("selected");
+        let saveButton = item.querySelector(".my-addresses__save");
+        saveButton.classList.remove("active");
+      });
+      this.classList.add("selected");
     });
-    this.classList.add("selected");
   });
-});
 
-const addressInputs = document?.querySelectorAll("#addressInput");
-addressInputs.forEach((item) => {
-  item.addEventListener("input", (event) => {
-    let saveButton = item.parentNode.parentNode.querySelector(
-      ".my-addresses__save"
-    );
-    saveButton.classList.add("active");
+  const addressInputs = document?.querySelectorAll("#addressInput");
+  addressInputs.forEach((item) => {
+    item.addEventListener("input", (event) => {
+      let saveButton = item.parentNode.parentNode.querySelector(
+        ".my-addresses__save"
+      );
+      saveButton.classList.add("active");
+    });
   });
-});
 
-const deleteButtons = document?.querySelectorAll(".my-adresses__delete");
-const modalMenu = document?.querySelector(".modal-menu");
-const bright80 = document?.querySelector(".bright80");
-console.log(bright80);
-deleteButtons.forEach((item) => {
-  item.addEventListener("click", (event) => {
-    modalMenu.classList.add("active");
-    bright80.classList.add("active");
+  const deleteButtons = document?.querySelectorAll(".my-adresses__delete");
+  const modalMenu = document?.querySelector(".modal-menu");
+  const bright80 = document?.querySelector(".bright80");
+  console.log(bright80);
+  deleteButtons.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      modalMenu.classList.add("active");
+      bright80.classList.add("active");
+    });
   });
-});
 
-const deleteButton = document?.querySelector(".modal-menu__delete-button");
-const cancelButton = document?.querySelector(".modal-menu__cancel-button");
+  const deleteButton = document?.querySelector(".modal-menu__delete-button");
+  const cancelButton = document?.querySelector(".modal-menu__cancel-button");
 
-deleteButton.addEventListener("click", (event) => {
-  modalMenu.classList.remove("active");
-  bright80.classList.remove("active");
-});
-cancelButton.addEventListener("click", (event) => {
-  modalMenu.classList.remove("active");
-  bright80.classList.remove("active");
+  deleteButton.addEventListener("click", (event) => {
+    modalMenu.classList.remove("active");
+    bright80.classList.remove("active");
+  });
+  cancelButton.addEventListener("click", (event) => {
+    modalMenu.classList.remove("active");
+    bright80.classList.remove("active");
+  });
 });
